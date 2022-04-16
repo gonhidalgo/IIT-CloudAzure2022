69a82bf935ac19d586d8bc801b65b84fe3d1c009
gonhidalgo <gonzaloh_98@hotmail.com>
2022-04-15 19:56:53 -0500

    Upload Project 6

---
+++ Project6/MortgageCalculatorFunction/Function1.cs
@@ -0,0 +1,96 @@
+using System.IO;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Attributes;
+using Microsoft.Azure.WebJobs.Extensions.OpenApi.Core.Enums;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using Newtonsoft.Json;
+using MortgageHelperNetCore31;
+
+namespace MortgageCalculatorFunction
+{
+    public static class Function1
+    {
+        [FunctionName("MonthlyPayment")]
+        [OpenApiOperation(operationId: "MonthlyPayment", tags: new[] { "MonthlyPayment" })]
+        [OpenApiSecurity("function_key", SecuritySchemeType.ApiKey, Name = "code", In = OpenApiSecurityLocationType.Query)]
+        [OpenApiParameter(name: "Principal", In = ParameterLocation.Query, Required = true, Type = typeof(double), Description = "The **Loan** parameter")]
+        [OpenApiParameter(name: "Rate", In = ParameterLocation.Query, Required = true, Type = typeof(double), Description = "The **Rate** parameter")]
+        [OpenApiParameter(name: "Years", In = ParameterLocation.Query, Required = true, Type = typeof(double), Description = "The **Years** parameter")]
+        [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "text/plain", bodyType: typeof(string), Description = "The OK response")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            string name = req.Query["name"];
+
+            string responseMessage = "";
+            double monthlyPayment = 0;
+
+            string principal = req.Query["Principal"];
+            string rate = req.Query["Rate"];
+            string years = req.Query["Years"];
+
+
+            if (string.IsNullOrEmpty(principal) ||
+                string.IsNullOrEmpty(rate) ||
+                string.IsNullOrEmpty(years))
+            {
+                responseMessage = "This HTTP triggered function executed successfully. It needs all the mortgage calculator arguments (Principal, rate, years).";
+                return new NotFoundObjectResult(responseMessage);
+            }
+            else
+            {
+                monthlyPayment = MortgageCalcHelper.ComputeMonthlyPayment(System.Convert.ToDouble(principal), System.Convert.ToDouble(years), System.Convert.ToDouble(rate));
+            }
+
+            return new OkObjectResult(monthlyPayment.ToString());
+        }
+
+        [FunctionName("MortgageCalc")]
+        [OpenApiOperation(operationId: "MortgageCalc", tags: new[] { "MortgageCalc" })]
+        [OpenApiSecurity("function_key", SecuritySchemeType.ApiKey, Name = "code", In = OpenApiSecurityLocationType.Query)]
+        [OpenApiParameter(name: "Principal", In = ParameterLocation.Query, Required = true, Type = typeof(double), Description = "The **Loan** parameter")]
+        [OpenApiParameter(name: "Rate", In = ParameterLocation.Query, Required = true, Type = typeof(double), Description = "The **Rate** parameter")]
+        [OpenApiParameter(name: "Years", In = ParameterLocation.Query, Required = true, Type = typeof(double), Description = "The **Years** parameter")]
+        [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK, contentType: "text/plain", bodyType: typeof(string), Description = "The OK response")]
+        public static IActionResult MortgageCalc(
+            [HttpTrigger(AuthorizationLevel.Function, "get", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            log.LogInformation("C# HTTP trigger function processed a request.");
+
+            string name = req.Query["name"];
+
+            string responseMessage = "";
+            double monthlyPayment = 0;
+
+            string principal = req.Query["Principal"];
+            string rate = req.Query["Rate"];
+            string years = req.Query["Years"];
+
+
+            if (string.IsNullOrEmpty(principal) ||
+                string.IsNullOrEmpty(rate) ||
+                string.IsNullOrEmpty(years))
+            {
+                responseMessage = "This HTTP triggered function executed successfully. It needs all the mortgage calculator arguments (Principal, rate, years).";
+                return new NotFoundObjectResult(responseMessage);
+            }
+            else
+            {
+                monthlyPayment = MortgageCalcHelper.ComputeMonthlyPayment(System.Convert.ToDouble(principal), System.Convert.ToDouble(years), System.Convert.ToDouble(rate));
+            }
+
+            return new OkObjectResult(monthlyPayment.ToString());
+        }
+    }
+}
+
---
+++ Project6/MortgageHelperCoreWebAPI/Controllers/MortgageCalcController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MortgageCalculator;
+
+namespace MortgageHelperCoreWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MortgageCalcController : ControllerBase
+    {
+        private readonly ILogger<MortgageCalcController> _logger;
+
+        public MortgageCalcController(ILogger<MortgageCalcController> logger)
+        {
+            _logger = logger;
+        }
+        
+        [HttpGet]
+        public double Get(double principal, double years, double rate)
+        {
+            return MortgageCalculator.MortgageCalcHelper.ComputeMonthlyPayment(principal, years, rate);
+        }
+
+
+
+    }
+}
